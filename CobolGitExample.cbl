d30cebff913ae87a3a87652ee3392c33995df571
DouglasTurner <33224803+DouglasTurner@users.noreply.github.com>
2018-02-15 13:42:36 -0600

    Add files via upload

---
+++ CobolGitExample.cbl
@@ -0,0 +1,19 @@
+       identification division.
+       program-id. CobolGitExample.
+
+       environment division.
+       configuration section.
+
+       data division.
+       working-storage section.
+
+       procedure division.
+
+
+           perform 1000-mainline.
+
+           goback.
+
+           1000-mainline.
+               display 'example'.
+       end program CobolGitExample.
